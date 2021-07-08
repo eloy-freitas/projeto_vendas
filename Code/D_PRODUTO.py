83ca3be855ae3c88efb1f39fd238c10d09550e77
eloy-freitas <freitaseloy.1@gmail.com>
2021-07-08 17:16:16 -0300

    testando outra abordagem de implementar a SCD PRODUTO

--- Code/D_PRODUTO.py
+++ Code/D_PRODUTO.py
@@ -1,9 +1,7 @@
-
 import pandas as pd
 import datetime as dt
 import time as t
 from CONEXAO import create_connection_postgre
-from tools import insert_data
 import DW_TOOLS as dwt
 
 columns_names = {
@@ -48,11 +46,25 @@ categoria_hortifruti = {"ALFACE", "CEBOLA", "ALHO", "TOMATE",
                         "BATATA DOCE", "BATATA INGLESA"}
 
 
+def extract_stage_produto(conn):
+    stage_produto = dwt.read_table(
+        conn=conn,
+        schema='STAGE',
+        table_name='STAGE_PRODUTO',
+        where=f'"id_produto" > 0 order by "id_produto";'
+    )
+
+    return stage_produto
+
+
 def extract_dim_produto(conn):
     dim_produto = dwt.read_table(
         conn=conn,
-        schema='STAGE',
-        table_name='STAGE_PRODUTO'
+        schema='DW',
+        table_name='D_PRODUTO',
+        where=f'"CD_PRODUTO" > 0 \
+        and "FL_ATIVO" = 1 \
+        order by "SK_PRODUTO";'
     )
 
     return dim_produto
@@ -79,7 +91,7 @@ def treat_dim_produto(dim_produto):
                 lambda y: float(y.replace(",", "."))),
             DT_CADASTRO=lambda x: x.DT_CADASTRO.apply(
                 lambda y: y[:10]),
-            DT_INICIO=lambda x: dt.date(1900, 1, 1),
+            DT_INICIO=lambda x: x.DT_CADASTRO,
             DT_FIM=lambda x: None).
             assign(
             DS_CATEGORIA=lambda x: x.NO_PRODUTO.map(
@@ -125,6 +137,174 @@ def load_dim_produto(dim_produto, conn):
     )
 
 
+def get_updated_produto(conn):
+    select_columns = [
+        "CD_PRODUTO",
+        "NO_PRODUTO",
+        "CD_BARRA",
+        "VL_PRECO_CUSTO",
+        "VL_PERCENTUAL_LUCRO"
+    ]
+
+    columns_to_update = {
+        "CD_BARRA",
+        "VL_PRECO_CUSTO",
+        "VL_PERCENTUAL_LUCRO"
+    }
+
+    df_stage = (
+            extract_stage_produto(conn).
+            rename(columns=columns_names).
+            assign(
+            VL_PRECO_CUSTO=lambda x:
+            x.VL_PRECO_CUSTO.apply(lambda y:
+                                   float(y.replace(",", "."))),
+            VL_PERCENTUAL_LUCRO=lambda x:
+            x.VL_PERCENTUAL_LUCRO.apply(lambda y:
+                                        float(y.replace(",", ".")))
+            )
+    )
+
+    df_dw = extract_dim_produto(conn)
+
+    concated_dfs = pd.concat([
+        df_dw.filter(items=select_columns),
+        df_stage.filter(items=select_columns)
+    ])
+    df = concated_dfs.reset_index(drop=True)
+    df_gpby = df.groupby(list(df.columns))
+    idx = [x[0] for x in df_gpby.groups.values() if len(x) == 1]
+    idx_dw = df.reindex(idx)['CD_PRODUTO'].unique()
+    old = (
+        df_dw.
+            query(f'CD_PRODUTO == {idx_dw}').
+            reset_index().
+            filter(items=select_columns)
+    )
+    new = (
+        df_stage.
+            query(f'CD_PRODUTO == {idx_dw}').
+            reset_index().
+            filter(items=select_columns)
+    )
+
+    if new['NO_PRODUTO'].item() != old['NO_PRODUTO'].item():
+        for cd in idx_dw:
+            nome = new.query(f"CD_PRODUTO == {cd}")["NO_PRODUTO"].item()
+            sql = f'update "DW"."D_PRODUTO"\
+                set "NO_PRODUTO" = \'{str(nome)}\'\
+                where "CD_PRODUTO" = {cd} and "FL_ATIVO" = 1;'
+            conn.execute(sql)
+    """
+    # descobrindo quais linhas foram alteradas
+    diference = (
+        df_dw.
+            filter(items=select_columns).
+            equals(df_stage.filter(items=select_columns),
+                    align_axis=0,
+                    keep_shape=False
+                    )
+    )
+
+    indexes = {x[0] for x in diference.index}
+    set_to_update = list(df_dw['CD_PRODUTO'].loc[indexes])
+    
+    if set(diference.columns).issubset(columns_to_update):
+        # identificando index das linhas alteradas
+        size = df_dw['SK_PRODUTO'].max() + 1
+
+        # extraindo linhas que serão atualizadas
+        updated_values = (
+            df_dw.loc[indexes].
+                assign(
+                SK_PRODUTO=lambda x: range(size, size
+                                           + len(indexes)),
+                CD_PRODUTO=lambda x: df_dw.loc[indexes]['CD_PRODUTO'],
+                DT_INICIO=lambda x: pd.to_datetime("today"),
+                DT_FIM=lambda x: None,
+                FL_ATIVO=lambda x: 1
+            )
+        )
+
+        for c in diference.columns:
+            updated_values[c] = diference.iloc[1][c]
+
+        # identificando as sks que precisam ser atualizadas 
+        for sk in set_to_update:
+            sql = f'update "DW"."D_PRODUTO"\
+                set "FL_ATIVO" = {0},\
+                "DT_FIM" = \'{pd.to_datetime("today")}\'\
+                where "CD_PRODUTO" = {sk};'
+            conn.execute(sql)
+
+        return updated_values
+
+    else:
+        if "NO_PRODUTO" in set(diference.columns):
+            print(diference['NO_PRODUTO'])
+            #print(diference.query("CD_PRODUTO == cd")["NO_PRODUTO"])
+            
+            for cd in set_to_update:
+                sql = f'update "DW"."D_PRODUTO"\
+                    set "MO_PRODUTO" = {diference.query("CD_PRODUTO = cd")},\
+                    "DT_FIM" = \'{pd.to_datetime("today")}\'\
+                    where "CD_PRODUTO" = {sk};'
+                conn.execute(sql)
+            
+    """
+
+def load_new_produto(insert_record, conn):
+    insert_record.to_sql(
+        con=conn,
+        name='D_PRODUTO',
+        schema='DW',
+        if_exists='append',
+        index=False,
+        chunksize=100
+    )   
+
+
+def run_dim_produto(conn):
+    (
+        extract_stage_produto(conn).
+            pipe(treat_dim_produto).
+            pipe(load_dim_produto, conn=conn)
+    )
+
+
+def run_new_produto(conn):
+    (
+        get_new_produto(conn).
+            pipe(load_new_produto, conn=conn)
+    )
+
+
+def run_update_produto(conn):
+    (
+        get_updated_produto(conn)#.
+            #pipe(load_new_produto, conn=conn)
+    )
+
+
+if __name__ == "__main__":
+    conn_dw = create_connection_postgre(
+        server="192.168.3.2",
+        database="projeto_dw_vendas",
+        username="itix",
+        password="itix123",
+        port="5432"
+    )
+
+    start = t.time()
+    #run_dim_produto(conn_dw)
+    #run_new_produto(conn_dw)
+    run_update_produto(conn_dw)
+    exec_time = t.time() - start
+    print(f"exec_time = {exec_time}")
+
+
+
+"""
 def get_new_produto(conn):
     df_stage = (
             dwt.read_table(
@@ -205,126 +385,5 @@ def get_new_produto(conn):
 
     return insert_record
 
+"""
 
-def get_updated_produto(conn):
-    select_columns = [
-        "CD_PRODUTO",
-        "NO_PRODUTO",
-        "CD_BARRA",
-        "VL_PRECO_CUSTO",
-        "VL_PERCENTUAL_LUCRO"
-    ]
-    df_stage = (
-        dwt.read_table(
-            conn=conn,
-            schema='STAGE',
-            table_name='STAGE_PRODUTO',
-            where='id_produto > 0 order by id_produto').
-            rename(columns=columns_names).
-            assign(
-            VL_PRECO_CUSTO=lambda x:
-            x.VL_PRECO_CUSTO.apply(lambda y:
-                                   float(y.replace(",", "."))),
-            VL_PERCENTUAL_LUCRO=lambda x:
-            x.VL_PERCENTUAL_LUCRO.apply(lambda y:
-                                        float(y.replace(",", "."))))
-    )
-
-    df_dw = dwt.read_table(
-        conn=conn,
-        schema='DW',
-        table_name='D_PRODUTO',
-        where=f'"CD_PRODUTO" > 0 order by "SK_PRODUTO";'
-    )
-
-    # descobrindo quais linhas foram alteradas
-    diference = (
-        df_dw.
-            filter(items=select_columns).
-            compare(df_stage.filter(items=select_columns),
-                    align_axis=0,
-                    keep_shape=False
-                    )
-    )
-
-    # identificando index das linhas alteradas
-    indexes = {x[0] for x in diference.index}
-    size = df_dw['SK_PRODUTO'].max() + 1
-
-    # extraindo linhas que serão atualizadas
-    updated_values = (
-        df_dw.loc[indexes].
-            assign(
-            SK_PRODUTO=lambda x: range(size, size
-                                       + len(indexes)),
-            CD_PRODUTO=lambda x: df_dw.loc[indexes]['CD_PRODUTO'],
-            DT_INICIO=lambda x: pd.to_datetime("today"),
-            DT_FIM=lambda x: None,
-            FL_ATIVO=lambda x: 1
-        )
-    )
-
-    for c in diference.columns:
-        updated_values[c] = diference.iloc[1][c]
-
-    # identificando as sks que precisam ser atualizadas
-    set_to_update = list(df_dw['CD_PRODUTO'].loc[indexes])
-
-    for sk in set_to_update:
-        sql = f'update "DW"."D_PRODUTO"\
-            set "FL_ATIVO" = {0},\
-            "DT_FIM" = \'{pd.to_datetime("today")}\'\
-            where "CD_PRODUTO" = {sk};'
-        conn.execute(sql)
-
-    return updated_values
-
-
-def load_new_produto(insert_record, conn):
-    insert_record.to_sql(
-        con=conn,
-        name='D_PRODUTO',
-        schema='DW',
-        if_exists='append',
-        index=False,
-        chunksize=100
-    )   
-
-
-def run_dim_produto(conn):
-    (
-        extract_dim_produto(conn).
-            pipe(treat_dim_produto).
-            pipe(load_dim_produto, conn=conn)
-    )
-
-
-def run_new_produto(conn):
-    (
-        get_new_produto(conn).
-            pipe(load_new_produto, conn=conn)
-    )
-
-
-def run_update_produto(conn):
-    (
-        get_updated_produto(conn).
-            pipe(load_new_produto, conn=conn)
-    )
-
-
-if __name__ == "__main__":
-    conn_dw = create_connection_postgre(
-        server="192.168.3.2",
-        database="projeto_dw_vendas",
-        username="itix",
-        password="itix123",
-        port="5432"
-    )
-
-    start = t.time()
-    run_dim_produto(conn_dw)
-    #run_new_produto(conn_dw)
-    #run_update_produto(conn_dw)
-    exec_time = t.time() - start
-    print(f"exec_time = {exec_time}")

8b6fcd64dea126899cc3ab2b37c9e45e8f01e010
eloy-freitas <freitaseloy.1@gmail.com>
2021-08-05 18:07:12 -0300

    fazendo tratamendo do fluxo de atualização de dados na fato

--- Code/F_VENDA.py
+++ Code/F_VENDA.py
@@ -5,8 +5,285 @@ from pandasql import sqldf
 from CONEXAO import create_connection_postgre
 import DW_TOOLS as dwt
 
-
+pd.set_option('display.max_columns', None)
 def extract_fact_venda(conn):
+    fact_venda = dwt.read_table(
+        conn=conn,
+        schema='DW',
+        table_name='F_VENDA',
+        columns=[
+            'SK_FORMA_PAGAMENTO',
+            'SK_CLIENTE',
+            'SK_FUNCIONARIO',
+            'SK_LOJA',
+            'SK_PRODUTO',
+            'SK_DT_VENDA',
+            'NU_NFC',
+            'QTD_PRODUTO',
+            'VL_PRECO_CUSTO',
+            'VL_PERCENTUAL_LUCRO'
+        ]
+    )
+
+    return fact_venda
+
+
+def extract_dim_forma_pagamento(conn):
+    dim_forma_pagamento = dwt.read_table(
+        conn=conn,
+        schema='DW',
+        table_name='D_FORMA_PAGAMENTO',
+        columns=["SK_FORMA_PAGAMENTO", "CD_FORMA_PAGAMENTO"]
+    )
+
+    return dim_forma_pagamento
+
+
+def extract_dim_cliente(conn):
+    dim_cliente = dwt.read_table(
+        conn=conn,
+        schema='DW',
+        table_name='D_CLIENTE',
+        columns=["SK_CLIENTE", "CD_CLIENTE"]
+    )
+
+    return dim_cliente
+
+
+def extract_dim_funcionario(conn):
+    dim_funcionario = dwt.read_table(
+        conn=conn,
+        schema='DW',
+        table_name='D_FUNCIONARIO',
+        columns=["SK_FUNCIONARIO", "CD_FUNCIONARIO"]
+    )
+
+    return dim_funcionario
+
+
+def extract_dim_data(conn):
+    dim_data = (
+        dwt.read_table(
+            conn=conn,
+            schema='DW',
+            table_name='D_DATA',
+            columns=['SK_DATA', 'DT_REFERENCIA']).
+            assign(
+            DT_REFERENCIA=lambda x:
+            pd.to_datetime(
+                x.DT_REFERENCIA,
+                format='%Y-%m-%d %H:%M:%S').
+                dt.strftime('%d-%m-%Y %H:%M:%S')).
+            assign(
+            DT_REFERENCIA=lambda x: x.DT_REFERENCIA.astype(str)).
+            assign(
+            DT_REFERENCIA=lambda x: x.DT_REFERENCIA.apply(
+                lambda y: y[:13])
+        )
+    )
+    return dim_data
+
+
+def extract_dim_produto(conn):
+    dim_produto = (
+        dwt.read_table(
+            conn=conn,
+            schema='DW',
+            table_name='D_PRODUTO',
+            columns=["SK_PRODUTO", "CD_PRODUTO",
+                     "VL_PRECO_CUSTO", "VL_PERCENTUAL_LUCRO",
+                     'DT_INICIO', 'DT_FIM']).
+            assign(
+            DT_INICIO=lambda x:
+            pd.to_datetime(
+                x.DT_INICIO,
+                format='%Y-%m-%d %H:%M:%S').
+                dt.strftime('%d-%m-%Y %H:%M:%S'),
+            DT_FIM=lambda x:
+            pd.to_datetime(
+                x.DT_FIM,
+                format='%Y-%m-%d %H:%M:%S').
+                dt.strftime('%d-%m-%Y %H:%M:%S'))
+    )
+
+    return dim_produto
+
+
+def extract_dim_loja(conn):
+    dim_loja = (
+        dwt.read_table(
+            conn=conn,
+            schema='DW',
+            table_name='D_LOJA',
+            columns=["SK_LOJA", "CD_LOJA", "DT_INICIO", "DT_FIM"]).
+            assign(
+            DT_INICIO=lambda x:
+            pd.to_datetime(
+                x.DT_INICIO,
+                format='%Y-%m-%d %H:%M:%S').
+                dt.strftime('%d-%m-%Y %H:%M:%S'),
+            DT_FIM=lambda x:
+            pd.to_datetime(
+                x.DT_FIM,
+                format='%Y-%m-%d %H:%M:%S').
+                dt.strftime('%d-%m-%Y %H:%M:%S'))
+
+    )
+
+    return dim_loja
+
+
+def extract_stage_venda(conn):
+    stage_venda = (
+        dwt.read_table(
+            conn=conn,
+            schema='STAGE',
+            table_name='STG_VENDA',
+            columns=['id_venda', 'id_pagamento', 'id_cliente',
+                     'id_func', 'id_loja', 'nfc', 'data_venda']).
+            assign(
+            data_venda=lambda x: pd.to_datetime(
+                x.data_venda,
+                format='%Y-%m-%d %H:%M:%S').
+                dt.strftime('%d-%m-%Y %H:%M:%S')).
+            assign(
+            data_referencia=lambda x: x.data_venda.astype(str)).
+            assign(
+            data_referencia=lambda x: x.data_referencia.apply(
+                lambda y: y[:13]),
+            id_venda=lambda x: x.id_venda.astype('int64'),
+            id_pagamento=lambda x: x.id_pagamento.astype('int64'),
+            id_cliente=lambda x: x.id_cliente.astype('int64'),
+            id_func=lambda x: x.id_func.astype('int64'),
+            id_loja=lambda x: x.id_loja.astype('int64')
+        ))
+
+    stage_item_venda = dwt.read_table(
+        conn=conn,
+        schema='STAGE',
+        table_name='STG_ITEM_VENDA',
+        columns=[
+            'id_venda',
+            'id_produto',
+            'qtd_produto'
+        ]
+    ).assign(
+        id_venda=lambda x: x.id_venda.astype('int64')
+    )
+
+    stg_venda = (
+        stage_venda.
+            pipe(pd.merge,
+                 right=stage_item_venda,
+                 left_on="id_venda",
+                 right_on="id_venda",
+                 suffixes=["_11", "_12"])
+    )
+
+    return stg_venda
+
+
+def extract_new_venda(conn):
+    stg_venda = extract_stage_venda(conn)
+
+    fact_venda = extract_fact_venda(conn)
+
+    new_values = (
+        sqldf('\
+            SELECT\
+            stg.id_venda,\
+            stg.id_pagamento,\
+            stg.id_cliente,\
+            stg.id_func,\
+            stg.id_loja,\
+            stg.nfc,\
+            stg.data_venda,\
+            stg.data_referencia,\
+            stg.id_produto,\
+            stg.qtd_produto\
+            FROM stg_venda stg\
+            LEFT JOIN fact_venda fact\
+            ON fact.NU_NFC = stg.nfc\
+            WHERE fact.NU_NFC IS NULL;'
+              )
+    )
+
+    if len(new_values) > 0:
+        dim_forma_pagamento = extract_dim_forma_pagamento(conn)
+        dim_cliente = extract_dim_cliente(conn)
+        dim_funcionario = extract_dim_funcionario(conn)
+        dim_loja = extract_dim_loja(conn)
+        dim_produto = extract_dim_produto(conn)
+        dim_data = extract_dim_data(conn)
+
+        fact_merged_dimensions = (
+            new_values.
+                pipe(dwt.merge_input,
+                     right=dim_data,
+                     left_on="data_referencia",
+                     right_on="DT_REFERENCIA",
+                     suff=['_16', '_17'],
+                     surrogate_key='SK_DATA').
+                pipe(dwt.merge_input,
+                     right=dim_forma_pagamento,
+                     left_on="id_pagamento",
+                     right_on="CD_FORMA_PAGAMENTO",
+                     suff=["_01", "_02"],
+                     surrogate_key="SK_FORMA_PAGAMENTO").
+                pipe(dwt.merge_input,
+                     right=dim_cliente,
+                     left_on="id_cliente",
+                     right_on="CD_CLIENTE",
+                     suff=["_03", "_04"],
+                     surrogate_key="SK_CLIENTE").
+                pipe(dwt.merge_input,
+                     right=dim_funcionario,
+                     left_on="id_func",
+                     right_on="CD_FUNCIONARIO",
+                     suff=["_05", "_06"],
+                     surrogate_key="SK_FUNCIONARIO")
+        )
+
+    print(fact_merged_dimensions)
+
+
+    merge_with_produto = (
+        sqldf('\
+        SELECT\
+        fmd.SK_FORMA_PAGAMENTO,\
+        fmd.id_produto,\
+        fmd.SK_CLIENTE,\
+        fmd.SK_FUNCIONARIO,\
+        fmd.SK_DATA,\
+        fmd.data_venda,\
+        fmd.qtd_produto,\
+        fmd.nfc, p.DT_INICIO, p.DT_FIM,\
+        p.SK_PRODUTO,\
+        p.VL_PRECO_CUSTO,\
+        p.VL_PERCENTUAL_LUCRO\
+        FROM fact_merged_dimensions fmd\
+        LEFT JOIN dim_produto p\
+        ON fmd.id_produto = p.CD_PRODUTO\
+        WHERE p.CD_PRODUTO = fmd.id_produto\
+        AND p.DT_INICIO <= fmd.data_venda <= p.DT_FIM;')
+    )
+
+    print(merge_with_produto)
+    
+    """
+    stg_venda = (
+        sqldf(f'SELECT\
+                fv.*, l.SK_LOJA \
+                FROM stg_venda fv\
+                LEFT JOIN lojas l on fv.id_loja = l.CD_LOJA\
+                WHERE l.CD_LOJA = fv.id_loja \
+                    AND l.SK_DT_INICIO <= fv.SK_DATA \
+                        AND fv.SK_DATA <= l.SK_DT_FIM;')
+    )
+    """
+
+
+def extract_stg_venda(conn):
     """
     Extrai todas as tabelas necessárias para gerar a fato venda
 
@@ -16,26 +293,27 @@ def extract_fact_venda(conn):
     return:
     stg_cliente_endereco -- pandas.Dataframe;
     """
-    stage_venda = (dwt.read_table(
-        conn=conn,
-        schema='STAGE',
-        table_name='STG_VENDA',
-        columns=['id_venda', 'id_pagamento', 'id_cliente',
-                 'id_func', 'id_loja', 'nfc', 'data_venda']).
-        assign(
-        data_venda=lambda x: pd.to_datetime(
-            x.data_venda,
-            format='%Y-%m-%d %H:%M:%S').
-            dt.strftime('%d-%m-%Y %H:%M:%S')).
-        assign(
-        data_venda=lambda x: x.data_venda.apply(
-            lambda y: y[:13]),
-        id_venda=lambda x: x.id_venda.astype('int64'),
-        id_pagamento=lambda x: x.id_pagamento.astype('int64'),
-        id_cliente=lambda x: x.id_cliente.astype('int64'),
-        id_func=lambda x: x.id_func.astype('int64'),
-        id_loja=lambda x: x.id_loja.astype('int64')
-    ))
+    stage_venda = (
+        dwt.read_table(
+            conn=conn,
+            schema='STAGE',
+            table_name='STG_VENDA',
+            columns=['id_venda', 'id_pagamento', 'id_cliente',
+                     'id_func', 'id_loja', 'nfc', 'data_venda']).
+            assign(
+            data_venda=lambda x: pd.to_datetime(
+                x.data_venda,
+                format='%Y-%m-%d %H:%M:%S').
+                dt.strftime('%d-%m-%Y %H:%M:%S')).
+            assign(
+            data_venda=lambda x: x.data_venda.apply(
+                lambda y: y[:13]),
+            id_venda=lambda x: x.id_venda.astype('int64'),
+            id_pagamento=lambda x: x.id_pagamento.astype('int64'),
+            id_cliente=lambda x: x.id_cliente.astype('int64'),
+            id_func=lambda x: x.id_func.astype('int64'),
+            id_loja=lambda x: x.id_loja.astype('int64')
+        ))
     stage_item_venda = dwt.read_table(
         conn=conn,
         schema='STAGE',
@@ -334,9 +612,9 @@ def run_fact_venda(conn):
     conn -- conexão criada via SqlAlchemy com o servidor do DW;
     """
     (
-        extract_fact_venda(conn).
-        pipe(treat_fact_venda).
-        pipe(load_fact_venda, conn=conn)
+        extract_stg_venda(conn).
+            pipe(treat_fact_venda).
+            pipe(load_fact_venda, conn=conn)
     )
 
 
@@ -349,5 +627,5 @@ if __name__ == '__main__':
         port="5432"
     )
     start = t.time()
-    run_fact_venda(conn_dw)
+    extract_new_venda(conn_dw)
     print(f'exec time = {t.time() - start}')
